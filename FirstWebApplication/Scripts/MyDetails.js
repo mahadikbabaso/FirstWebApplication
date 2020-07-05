4c66e5bbed07780e439f440d8eef4865ed3b243d
babaso.mahadik <babaso.mahadik@northgateps.com>
2020-07-05 16:41:49 +0530

    Test chanegs

---
+++ FirstWebApplication/Scripts/MyDetails.js
@@ -0,0 +1,15 @@
+﻿function valuecheck() {
+    var firstname = document.getElementById("txtFirstName").value;
+    var lastName = document.getElementById("txtLastName").value;
+    if (firstname == "") {
+        document.getElementById("lblResult").innerHTML = "Please enter first name";
+
+        return false;
+    }
+    if (lastName == "") {
+        document.getElementById("lblResult").innerHTML = "Please enter last name";
+
+        return false;
+    }
+    document.getElementById("lblResult").innerHTML = "My name is : " + document.getElementById("txtFirstName").value + " " + document.getElementById("txtLastName").value;
+}
